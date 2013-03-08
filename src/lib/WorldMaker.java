c6a8af8205bda3d9261154e5f3982277faabf66d
nwiehoff <nwiehoff@HPE-460Z>
2013-03-08 06:47:04 -0500

    * world maker can now generate decent connections between systems 	-> does not guarantee there won't be islands not connected to the rest of the world, but that is going to be fixed in post.

--- src/lib/WorldMaker.java
+++ src/lib/WorldMaker.java
@@ -16,6 +16,8 @@ import lib.Parser.Term;
 public class WorldMaker {
 
     public WorldMaker() {
+        /*//sysling test
+         syslingTest();*/
         //generate universe
         String out = generate(1, 10, 80, 100, 100, 40000, 120000, 500, 1500, 1, 3, 25000, 200000);
         //save
@@ -39,7 +41,7 @@ public class WorldMaker {
             Parser stars = new Parser("PLANET.txt");
             ArrayList<Term> starTypes = stars.getTermsOfType("Star");
             /*Parser particle = new Parser("PARTICLE.txt");
-            ArrayList<Term> nebTypes = particle.getTermsOfType("Nebula");*/
+             ArrayList<Term> nebTypes = particle.getTermsOfType("Nebula");*/
             Parser planets = new Parser("PLANET.txt");
             ArrayList<Term> planetTypes = planets.getTermsOfType("Planet");
             //start rng
@@ -49,14 +51,44 @@ public class WorldMaker {
             if (numSystems < minSystems) {
                 numSystems = minSystems;
             }
-            //generate each system
+            //generate syslings
+            ArrayList<Sysling> syslings = new ArrayList<>();
             for (int a = 0; a < numSystems; a++) {
+                //determine map location
+                double x = rnd.nextInt(worldSize * 2) - worldSize;
+                double y = rnd.nextInt(worldSize * 2) - worldSize;
+                //pick name
+                String name = "System " + a;
+                //make sysling
+                Sysling test = new Sysling(name, new Point2D.Double(x, y));
+                //check for collission
+                boolean safe = true;
+                for (int v = 0; v < syslings.size(); v++) {
+                    if (syslings.get(v).collideWith(test)) {
+                        safe = false;
+                        break;
+                    }
+                }
+                //add if safe
+                if (safe) {
+                    syslings.add(test);
+                }
+            }
+            //stort syslings
+            for (int a = 0; a < syslings.size(); a++) {
+                syslings.get(a).sortBuddy(syslings);
+            }
+            //generate each system
+            for (int a = 0; a < syslings.size(); a++) {
                 System.out.println("Generating Universe - " + (a + 1) + "/" + numSystems);
                 ArrayList<Simpling> objects = new ArrayList<>();
                 String thisSystem = "";
                 {
-                    //pick name
-                    String systemName = "System " + a;
+                    //get sysling
+                    Sysling sys = syslings.get(a);
+                    double x = sys.getLoc().x;
+                    double y = sys.getLoc().y;
+                    String systemName = sys.getName();
                     //pick size
                     int size = rnd.nextInt(maxSystemSize);
                     if (size < minSystemSize) {
@@ -64,12 +96,9 @@ public class WorldMaker {
                     }
                     //determine skybox
                     int pick = rnd.nextInt(skyTypes.size());
-                    //determine map location
-                    double x = rnd.nextInt(worldSize * 2) - worldSize;
-                    double y = rnd.nextInt(worldSize * 2) - worldSize;
                     //add some randomization
-                    x += 2.0*rnd.nextDouble()-1;
-                    y += 2.0*rnd.nextDouble()-1;
+                    x += 2.0 * rnd.nextDouble() - 1;
+                    y += 2.0 * rnd.nextDouble() - 1;
                     //create the system entry
                     thisSystem +=
                             "[System]\n"
@@ -95,58 +124,61 @@ public class WorldMaker {
                             + "texture=" + starTypes.get(pick).getValue("name") + "\n"
                             + "x=" + x + "\n"
                             + "y=" + y + "\n"
-                            + "d=" + 2*r + "\n"
+                            + "d=" + 2 * r + "\n"
                             + "seed=" + seed + "\n"
                             + "[/Star]\n\n";
                     //add a simpling for testing
-                    objects.add(new Simpling(new Point2D.Float((float)x,(float)y), 4 * r));
+                    objects.add(new Simpling(new Point2D.Float((float) x, (float) y), 4 * r));
                     /*
-                     * CREATE NEBULA
-                     * 
+                     * CREATE JUMPHOLES
                      */
-                    /*int numNebula = rnd.nextInt(maxNebulaPerSystem);
-                    if (numNebula < minNebulaPerSystem) {
-                        numNebula = minNebulaPerSystem;
-                    }
-                    for (int b = 0; b < numNebula; b++) {
-                        //pick texture
-                        pick = rnd.nextInt(nebTypes.size());
-                        Term type = nebTypes.get(pick);
-                        //pick name
-                        String name = "Nebula " + b;
-                        //pick position
-                        x = rnd.nextInt(size * 4) - size * 2;
-                        y = 0;
-                        z = rnd.nextInt(size * 4) - size * 2;
-                        //pick dimension
-                        r = rnd.nextInt(Math.min(2 * size, maxNebulaSize));
-                        if (r < minNebulaSize) {
-                            r = minNebulaSize;
+                    //calculate the number of connections to make
+                    int density = rnd.nextInt(4) + 1;
+                    for (int v = 0; v < density; v++) {
+                        //get the sysling to connect to
+                        Sysling in = sys;
+                        Sysling out = sys.findBuddy(null, v + 1);
+                        if (!in.connectedTo(out) && !out.connectedTo(in)) {
+                            //name holes
+                            String inName = out.getName() + " Jumphole";
+                            String outName = sys.getName() + " Jumphole";
+                            //build a bridge
+                        /*
+                             * [Jumphole]
+                             name=System 9 Gate
+                             system=System 16
+                             out=System 9/System 16 Gate
+                             x=-6000
+                             y=-2000
+                             [/Jumphole]
+                             */
+                            //build in gate
+                            x = rnd.nextInt(size * 2) - size;
+                            y = rnd.nextInt(size * 2) - size;
+                            thisSystem +=
+                                    "[Jumphole]\n"
+                                    + "name=" + inName + "\n"
+                                    + "x=" + x + "\n"
+                                    + "y=" + y + "\n"
+                                    + "system=" + in.getName() + "\n"
+                                    + "out=" + out.getName() + "/" + outName + "\n"
+                                    + "[/Jumphole]\n\n";
+                            //build out gate
+                            x = rnd.nextInt(size * 2) - size;
+                            y = rnd.nextInt(size * 2) - size;
+                            thisSystem +=
+                                    "[Jumphole]\n"
+                                    + "name=" + outName + "\n"
+                                    + "x=" + x + "\n"
+                                    + "y=" + y + "\n"
+                                    + "system=" + out.getName() + "\n"
+                                    + "out=" + in.getName() + "/" + inName + "\n"
+                                    + "[/Jumphole]\n\n";
+                            //inform simplings
+                            sys.addConnection(out);
+                            out.addConnection(sys);
                         }
-                        int hScale = rnd.nextInt(3) + 2;
-                        int h = r / hScale;
-                        //pick color
-                        float red = rnd.nextFloat() / 2;
-                        float green = rnd.nextFloat() / 2;
-                        float blue = rnd.nextFloat() / 2;
-                        //seed
-                        seed = rnd.nextInt();
-                        //generate entry
-                        thisSystem +=
-                                "[Nebula]\n"
-                                + "name=" + name + "\n"
-                                + "system=" + systemName + "\n"
-                                + "type=" + type.getValue("name") + "\n"
-                                + "color=" + red + "," + green + "," + blue + "," + 1 + "\n"
-                                + "x=" + x + "\n"
-                                + "y=" + y + "\n"
-                                + "z=" + z + "\n"
-                                + "l=" + r + "\n"
-                                + "w=" + h + "\n"
-                                + "h=" + r + "\n"
-                                + "seed=" + seed + "\n"
-                                + "[/Nebula]\n\n";
-                    }*/
+                    }
                     /*
                      * CREATE PLANETS
                      */
@@ -173,7 +205,7 @@ public class WorldMaker {
                             r = minPlanetSize;
                         }
                         //create a simpling for testing
-                        Simpling test = new Simpling(new Point2D.Float((float)x,(float)y), r);
+                        Simpling test = new Simpling(new Point2D.Float((float) x, (float) y), r);
                         boolean safe = true;
                         for (int c = 0; c < objects.size(); c++) {
                             if (objects.get(c).collideWith(test)) {
@@ -190,7 +222,7 @@ public class WorldMaker {
                                     + "texture=" + texture + "\n"
                                     + "x=" + x + "\n"
                                     + "y=" + y + "\n"
-                                    + "d=" + 2*r + "\n"
+                                    + "d=" + 2 * r + "\n"
                                     + "seed=" + seed + "\n"
                                     + "[/Planet]\n\n";
                         }
@@ -200,6 +232,104 @@ public class WorldMaker {
             }
         }
         return ret;
+    }
+
+    private void syslingTest() {
+        ArrayList<Sysling> test = new ArrayList<>();
+        Random rnd = new Random(5);
+        //make some syslings
+        for (int a = 0; a < 50; a++) {
+            String name = "Sysling " + a;
+            double sx = rnd.nextInt(1000) - 500;
+            double sy = rnd.nextInt(1000) - 500;
+            test.add(new Sysling(name, new Point2D.Double(sx, sy)));
+        }
+        //make them find their buddy
+        for (int a = 0; a < test.size(); a++) {
+            test.get(a).sortBuddy(test);
+        }
+        //get a print out
+        Sysling t = test.get(0);
+        System.out.println("Mapping from " + t.getName());
+        for (int a = 0; a < test.size(); a++) {
+            Sysling _0th = t.findBuddy(test, a);
+            System.out.println(_0th.getName() + " " + _0th.distance(t));
+        }
+    }
+
+    public class Sysling {
+        /*
+         * Solar system template used for jump hole mapping.
+         */
+
+        private Point2D.Double loc;
+        private Sysling[] neighbors;
+        private ArrayList<Sysling> connections = new ArrayList<>();
+        private String name;
+
+        public Sysling(String name, Point2D.Double loc) {
+            this.loc = loc;
+            this.name = name;
+        }
+
+        public void addConnection(Sysling sys) {
+            connections.add(sys);
+        }
+
+        public boolean connectedTo(Sysling test) {
+            return connections.contains(test);
+        }
+
+        public void sortBuddy(ArrayList<Sysling> verse) {
+            /*
+             * Sorts the syslings by distance ascending.
+             */
+            Sysling[] arr = (Sysling[]) verse.toArray(new Sysling[0]);
+            for (int a = 0; a < arr.length; a++) {
+                for (int b = 1; b < arr.length - a; b++) {
+                    if (distance(arr[b - 1]) > distance(arr[b])) {
+                        Sysling tmp = arr[b];
+                        arr[b] = arr[b - 1];
+                        arr[b - 1] = tmp;
+                    }
+                }
+            }
+            neighbors = arr;
+        }
+
+        public Sysling findBuddy(ArrayList<Sysling> verse, int place) {
+            /*
+             * Finds Nth closest sysling to this one. Nth = place.
+             */
+            if (neighbors == null) {
+                sortBuddy(verse);
+            }
+            return neighbors[place];
+        }
+
+        public boolean collideWith(Sysling test) {
+            if (test.getName().matches(name)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public double distance(Sysling comp) {
+            return loc.distance(comp.getLoc());
+        }
+
+        public Point2D.Double getLoc() {
+            return loc;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String toString() {
+            return name;
+        }
     }
 
     public class Simpling {
@@ -217,7 +347,7 @@ public class WorldMaker {
         }
 
         public boolean collideWith(Simpling test) {
-            Point2D.Float testLoc = (Point2D.Float)test.getLoc().clone();
+            Point2D.Float testLoc = (Point2D.Float) test.getLoc().clone();
             float testRad = test.getRad();
             //add the radii to get the min separation
             float minRad = testRad + rad;
