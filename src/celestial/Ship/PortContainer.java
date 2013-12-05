4d0929fba59c7fbd93197b35a0d6375ddd8c752b
nwiehoff <nwiehoff@nwiehoff-PC>
2013-12-05 01:55:32 -0500

    * increased station hold limit from 20->40 minutes (ingame).

--- src/celestial/Ship/PortContainer.java
+++ src/celestial/Ship/PortContainer.java
@@ -34,7 +34,7 @@ public class PortContainer implements Serializable {
     protected Station parent;
     private Ship client;
     //timing
-    private double maxHold = 1200;
+    private double maxHold = 2400;
     private double time = 0;
 
     public PortContainer(Station parent, double x, double y, int w, int h, double ax, double ay) {
