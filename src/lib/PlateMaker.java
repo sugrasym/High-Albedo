43030b7edb8d3b369391b2b503b69c119b693ff5
nwiehoff <nwiehoff@nwiehoff-PC.gateway.2wire.net>
2014-01-15 05:07:21 -0500

    * wrote a program to batch convert nebula backplates into both std and wide formats

---
+++ src/lib/PlateMaker.java
@@ -0,0 +1,134 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Program for automatically converting a set of images into widescreen and
+ * standard backplates.
+ */
+package lib;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author nwiehoff
+ */
+public class PlateMaker {
+
+    private static final int wideX = 1920;
+    private static final int wideY = 1080;
+    private static final int stdX = 1280;
+    private static final int stdY = 1024;
+
+    private String name;
+    private String inputDirectory;
+    private String outputDirectory;
+
+    public PlateMaker() {
+        if (setup()) {
+            File inDir = new File(inputDirectory);
+            //create std and wide subfolders
+            File outDir = new File(outputDirectory);
+            String stdPath = outputDirectory + "/std/";
+            String widePath = outputDirectory + "/wide/";
+            File stdOutDir = new File(stdPath);
+            File wideOutDir = new File(widePath);
+            if (!outDir.exists()) {
+                outDir.mkdir();
+            }
+            if (!stdOutDir.exists()) {
+                stdOutDir.mkdir();
+            }
+            if (!wideOutDir.exists()) {
+                wideOutDir.mkdir();
+            }
+            //get a list of files in the directory
+            File[] listOfFiles = inDir.listFiles();
+            for (int a = 0; a < listOfFiles.length; a++) {
+                File raw = listOfFiles[a];
+                try {
+                    //load the file into memory
+                    Image ico = loadImage(raw);
+                    BufferedImage buff = new BufferedImage(ico.getWidth(null), ico.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+                    //draw
+                    Graphics z = buff.getGraphics();
+                    z.drawImage(ico, 0, 0, null);
+                    //create the std subsection
+                    int sx = (ico.getWidth(null) / 2) - (stdX / 2);
+                    int sy = (ico.getHeight(null) / 2) - (stdY / 2);
+                    BufferedImage std = buff.getSubimage(sx, sy, stdX, stdY);
+                    //create the wide subsection
+                    int wx = (ico.getWidth(null) / 2) - (wideX / 2);
+                    int wy = (ico.getHeight(null) / 2) - (wideY / 2);
+                    BufferedImage wide = buff.getSubimage(wx, wy, wideX, wideY);
+                    //save the std image
+                    File stdOut = new File(stdPath + name + a + ".png");
+                    ImageIO.write(std, "png", stdOut);
+                    //save the wide image
+                    File wideOut = new File(widePath + name + a + ".png");
+                    ImageIO.write(wide, "png", wideOut);
+                    //report
+                    System.out.println("Finished processing file " + (a + 1) + " / " + listOfFiles.length);
+                } catch (Exception e) {
+                    System.out.println("Failed to convert image " + raw);
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            System.out.println("Aborted.");
+        }
+    }
+
+    private boolean setup() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input directory: ");
+        inputDirectory = scan.nextLine();
+        System.out.print("Output directory: ");
+        outputDirectory = scan.nextLine();
+        System.out.print("Name: ");
+        name = scan.nextLine();
+        //verify
+        System.out.println("You have selected");
+        System.out.println("In:   " + inputDirectory);
+        System.out.println("Out:  " + outputDirectory);
+        System.out.println("Name: " + name);
+        System.out.println("Y/N?: ");
+        String pick = scan.nextLine();
+        if (pick.trim().toUpperCase().matches("Y")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Image loadImage(File target) throws NullPointerException, URISyntaxException {
+        System.out.println("Loading image resource " + target);
+        ImageIcon ico = new ImageIcon(target.getAbsolutePath());
+        //extract image
+        Image test = ico.getImage();
+        return test;
+    }
+
+    public static void main(String[] args) {
+        new PlateMaker();
+    }
+}
